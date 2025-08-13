460486abc1828375e7b9475c920ec4bebd8cc560
killrealpp <kaysarovkirill5@gmail.com>
2025-08-13 14:06:26 +0300

    add fixs

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-document.getElementById('email').addEventListener('click', ()=>{
-    const email = this.textContent
-    navigator.clipboard.writeText(email)
-        .then(()=>{
-            alert('Email скопирован: ' + email);
-        })
+let lastScroll = 0
+const header = document.querySelector('.header')
+const scrollHold = 5
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.scrollY
+    if (Math.abs(currentScroll - lastScroll) < scrollHold) {
+    }
+    if (currentScroll > lastScroll && currentScroll > header.offsetHeight) {
+        header.classList.add('hide')
+    } else {
+        header.classList.remove('hide')
+    }
+    lastScroll = currentScroll
 })
